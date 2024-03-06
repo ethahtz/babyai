58fe2cd07cba328380a77343f6691947c9c7b934
Tianze (Etha) Hua <61071349+ethahtz@users.noreply.github.com>
2024-03-05 21:22:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     packages=['babyai', 'babyai.levels', 'babyai.utils'],
     install_requires=[
-        'gym>=0.9.6',
+        'gym==0.17.2',
         'numpy==1.15.4', # Temporary: fix numpy version because of bug introduced in 1.16
         "torch>=0.4.1",
         'blosc>=1.5.1',
